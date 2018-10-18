01068eedcd6e6c0a48dac6178861acfd5c341689
Vincent Bacalso <bacalso.vincent@gmail.com>
2018-10-18 10:31:11 +0800

    - added endpoints for networking in project

---
+++ StandardCatalog/StandardCatalog/Networking/Reqres.swift
@@ -0,0 +1,24 @@
+//
+//  Reqres.swift
+//  StandardCatalog
+//
+//  Created by Vincent Bacalso on 18/10/2018.
+//  Copyright © 2018 CommandBin. All rights reserved.
+//
+
+import Foundation
+
+// This is the endpoints data from reqres free service https://reqres.in/
+struct Reqres {
+  
+  static let domain = "https://reqres.in"
+  
+  static let users = 	"/api/users?page=2"
+  static let user = "/api/users/2"
+  
+  // Create, Update, Patch, Delete
+  
+  static let register = "/api/register"
+  static let login = "/api/login"
+  
+}

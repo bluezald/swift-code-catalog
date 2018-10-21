9f98adfaeb5114a32a8475696cf2767db419a844
Vincent Bacalso <bacalso.vincent@gmail.com>
2018-10-21 21:55:41 +0800

    - added collection view controller

---
+++ StandardCatalog/StandardCatalog/UIKit/CollectionView/SimpleCollectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SimpleCollectionViewController.swift
+//  StandardCatalog
+//
+//  Created by Vincent Bacalso on 21/10/2018.
+//  Copyright © 2018 CommandBin. All rights reserved.
+//
+
+import Foundation

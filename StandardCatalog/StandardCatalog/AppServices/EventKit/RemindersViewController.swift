ad185a4017c4709ee589d1a64f8832ff0ac9bb24
Vincent Bacalso <bacalso.vincent@gmail.com>
2018-10-30 21:17:21 +0800

    - updated reminders view to access EventStore

--- StandardCatalog/StandardCatalog/AppServices/EventKit/RemindersViewController.swift
+++ StandardCatalog/StandardCatalog/AppServices/EventKit/RemindersViewController.swift
@@ -16,14 +16,20 @@ import EventKit
 class RemindersViewController: UITableViewController {
   
   let eventStore = EKEventStore()
+  var calendar: EKCalendar?
   var items = [String]()
   
-  func accessCalendar() {
-    let status = EKEventStore.authorizationStatus(for: EKEntityType.event)
+  func accessReminders() {
+    let status = EKEventStore.authorizationStatus(for: .reminder)
     
     switch (status) {
     case .notDetermined:
       print("Not determined")
+      self.eventStore.requestAccess(to: .reminder) { [weak self] (granted, error) in
+        DispatchQueue.main.async {
+          self?.tableView.reloadData()
+        }
+      }
     case .authorized:
       tableView.reloadData()
     case .restricted, .denied:
@@ -39,7 +45,7 @@ extension RemindersViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
-    
+    accessReminders()
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {

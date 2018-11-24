b6278cc8474f0a644cd783dcbcfe250ddc467e56
Vincent Bacalso <bacalso.vincent@gmail.com>
2018-11-24 20:17:38 +0800

    - added initial file for keychain service sample usage

---
+++ StandardCatalog/StandardCatalog/Security/KeychainServicesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  KeychainServicesViewController.swift
+//  StandardCatalog
+//
+//  Created by Vincent Bacalso on 24/11/2018.
+//  Copyright © 2018 CommandBin. All rights reserved.
+//
+
+import Foundation
+
+///
+/// This is a sample usage to the problem here:
+/// https://bit.ly/2DETIkW
+/// TODO: Present a view controller with a mocked login ui
+/// to demonstrate saving to keychain.
+///

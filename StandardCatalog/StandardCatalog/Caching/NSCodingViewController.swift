df61fba2521210f779ec6735b9154ddca5829e67
Vincent Bacalso <bacalso.vincent@gmail.com>
2018-12-02 15:06:42 +0800

    - applied nscoding in object in NSCoding sample

--- StandardCatalog/StandardCatalog/Caching/NSCodingViewController.swift
+++ StandardCatalog/StandardCatalog/Caching/NSCodingViewController.swift
@@ -10,7 +10,12 @@ import Foundation
 
 /// This sample is base on https://www.raywenderlich.com/6733-nscoding-tutorial-for-ios-how-to-permanently-save-app-data
 
-class Creature: NSObject {
+enum Keys: String {
+  case title = "Title"
+  case rating = "Rating"
+}
+
+class Creature: NSObject, NSCoding {
   var title = ""
   var rating: Float = 0
   
@@ -19,4 +24,16 @@ class Creature: NSObject {
     self.title = title
     self.rating = rating
   }
+  
+  func encode(with aCoder: NSCoder) {
+    aCoder.encode(title, forKey: Keys.title.rawValue)
+    aCoder.encode(rating, forKey: Keys.rating.rawValue)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    let title = aDecoder.decodeObject(forKey: Keys.title.rawValue) as! String
+    let rating = aDecoder.decodeFloat(forKey: Keys.rating.rawValue)
+  }
+  
+  
 }
